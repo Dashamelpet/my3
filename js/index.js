e86ed6259da8713c8d19885c48eada61cfaabf97
Melnik <dashapetrash@gmail.com>
2023-10-27 21:22:50 +0300

    add preloader

--- js/index.js
+++ js/index.js
@@ -113,3 +113,7 @@ let menuList = document.querySelector(".menu-list")
 menuBtn.addEventListener("click", function(){
     menuList.classList.toggle("open");
 });
+
+//preloader
+
+var hellopreloader = document.getElementById("hellopreloader_preload");function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
